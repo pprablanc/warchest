4507389147b00576d650dbed7a18cacd0538ae94
pprablanc <pgit@net-c.com>
2021-03-12 17:01:40 +0100

    Board, Player classes

--- warchest/classes.py
+++ warchest/classes.py
@@ -3,38 +3,55 @@
 import pandas as pd
 import random
 
-class board(object):
+class Board(object):
 
-    def __init__(self):
+    def __init__(self, player1, player2, draft='random'):
+        # load data
         self._board_initialization()
+        self._load_units()
+
+        # make draft
+        if draft == 'random':
+            (unit_set, remaining_unit_list) = self._random_unit_set()
+            player1.set_unit_set(unit_set)
+            (unit_set, _) = self._random_unit_set(remaining_unit_list)
+            player2.set_unit_set(unit_set)
+        else:
+            assert(draft == 'random'), "Manual draft not yet implemented"
+
+        # Set initiative
+        self.initiative = random.randint(0, 1) # 0: Player 2 has initiative
+
 
     def _board_initialization(self):
-        filename = 'board.csv'
+        filename = '../resources/board.csv'
         try:
             self.hexagon = pd.read_csv(filename, sep='\t')
         except:
             print("board.csv file was not found.")
 
-    # other function interacting with players ?
-
-class player(object):
-
-    def __init__(self, remaining_unit_list):
-        self.units = self._load_units()
-        (self.unit_set, self.remaining_unit_list) = self._random_unit_set(remaining_unit_list)
-
-    def _load_units():
-        filename = 'units.csv'
+    def _load_units(self):
+        filename = '../resources/units.csv'
         try:
-            units = pd.read_csv(filename, sep='\t')
+            self.units = pd.read_csv(filename, sep='\t')
         except:
             print("units.csv file was not found.")
 
-    def _random_unit_set(self, remaining_unit_list):
+    def _random_unit_set(self, remaining_unit_list=[]):
         if remaining_unit_list:
             unit_list = remaining_unit_list
         else:
-            unit_list = list(self.units['id_unit'])
+            unit_list = list(self.units['id'])
         unit_set = random.sample(unit_list, 4)
         remaining_unit_list = list(set(unit_list) - set(unit_set))
         return (unit_set, remaining_unit_list)
+
+    # other function interacting with players ?
+
+class Player(object):
+
+    def __init__(self):
+        self.unit_set = []
+
+    def set_unit_set(self, unit_list):
+        self.unit_set = unit_list
--- warchest/main.py
+++ warchest/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 
-
+from classes import Board, Player
 
 def main():
-    pass
+    p1 = Player()
+    p2 = Player()
+    board = Board(p1, p2)
+    import pdb; pdb.set_trace()
 
 if __name__ == '__main__':
     main()
